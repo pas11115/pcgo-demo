a6c6a515c31a57a3b38eae53c3a11bc42831b024
prakash <pas11115@gmail.com>
2019-10-05 01:18:37 +0530

    first commit: adds gitignore

---
+++ pmain.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Println(t.Format("20060102150405"))
+	fmt.Fprintf(w, "My Awesome Go App")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+}
+
+func main() {
+	fmt.Println("Go Web App Started on Port 3000")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
+}
